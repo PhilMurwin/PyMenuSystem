ff8c217cb29c1f7be1d9365ddd98233ab20e6ad4
PhilMurwin <Phil.Murwin@pointbsolutions.com>
2022-03-20 00:29:57 -0500

    Initial Commit of PyMenuSystem
    - Setup Game Loop
    - Opens blank window

---
+++ game.py
@@ -0,0 +1,41 @@
+import pygame
+
+class Game():
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption('PyMenuSystem')
+        self.running, self.playing = True, False
+        self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
+        self.DISPLAY_W, self.DISPLAY_H = 480, 270
+        self.display = pygame.Surface((self.DISPLAY_W,self.DISPLAY_H))
+        self.window = pygame.display.set_mode((self.DISPLAY_W,self.DISPLAY_H))
+        self.font_name = 'assets/font/8-BIT WONDER.TTF'
+        #self.font_name = pygame.font.get_default_font()
+        self.BLACK, self.WHITE = (0,0,0), (255,255,255)
+    
+    def game_loop(self):
+        while self.playing:
+            self.check_events()
+            if self.START_KEY:
+                self.playing = False
+            self.display.fill(self.BLACK)
+            self.window.blit(self.display, (0,0))
+            pygame.display.update()
+            self.reset_keys()
+
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running, self.playing = False, False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    self.START_KEY = True
+                if event.key == pygame.K_BACKSPACE:
+                    self.BACK_KEY = True
+                if event.key == pygame.K_DOWN:
+                    self.DOWN_KEY = True
+                if event.key == pygame.K_UP:
+                    self.UP_KEY = True
+    
+    def reset_keys(self):
+        self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
